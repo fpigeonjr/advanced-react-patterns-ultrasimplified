1a47d96cf3c7891392739ca6905736bf5c969933
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-02-24 17:10:46 -0600

    adds button clap animation

--- showcase/src/patterns/01.js
+++ showcase/src/patterns/01.js
@@ -11,10 +11,26 @@ const initialState = {
 // Higher Order Component
 const withClapAnimation = WrappedComponent => {
   class WithClapAnimation extends Component {
+    animationTimeline = new mojs.Timeline();
+
     state = {
-      animationTimeline: new mojs.Timeline()
+      animationTimeline: this.animationTimeline
     };
     // handles animation logic
+
+    componentDidMount() {
+      const scaleButton = new mojs.Html({
+        el: "#clap",
+        duration: 300,
+        scale: { 1.3: 1 },
+        easing: mojs.easing.ease.out
+      });
+
+      const newAnimationTimeline = this.animationTimeline.add([scaleButton]);
+      this.setState({
+        animationTimeline: newAnimationTimeline
+      });
+    }
 
     render() {
       return (
@@ -46,7 +62,7 @@ const MediumClap = ({ animationTimeline }) => {
   };
 
   return (
-    <button className={styles.clap} onClick={handleClapClick}>
+    <button id="clap" className={styles.clap} onClick={handleClapClick}>
       <ClapIcon isClicked={isClicked} />
       <ClapCount count={count} />
       <CountTotal countTotal={countTotal} />
